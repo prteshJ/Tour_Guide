460a7a85a0e246a3aecebd5f6fd7e05ac7f00f86
Dark Cool Wizard <priteshjakhotia@gmail.com>
2017-04-08 13:54:45 -0500

    Update AttractionsFragment.java
    Prevented recycler view from duplicating items! I made my  array list final and its scope from global to local.

--- app/src/main/java/com/example/android/tour_guide/AttractionsFragment.java
+++ app/src/main/java/com/example/android/tour_guide/AttractionsFragment.java
@@ -16,10 +16,9 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ThingsToDoFragment extends Fragment {
-    ArrayList<Location> things_to_do = new ArrayList<>(); // Contains all things to do
+public class AttractionsFragment extends Fragment {
 
-    public ThingsToDoFragment() {
+    public AttractionsFragment() {
         // Required empty public constructor
     }
 
@@ -27,25 +26,21 @@ public class ThingsToDoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflates the layout file which contains recycler view so that we can use it
         View rootView = inflater.inflate(R.layout.location_list, container, false);
 
         RecyclerView placesList = (RecyclerView) rootView.findViewById(R.id.placesList);
 
-        // Populate list of things to do around Chicago using setters to prevent recycler view
-        // duplicating views which happened earlier when I used add method
-        things_to_do.set(0, new Location(getResources(), R.drawable.chicago_360, getString(R.string.streeterville_location), getString(R.string.things_to_do_1)));
-        things_to_do.set(1, new Location(getResources(), R.drawable.art_institute_of_chicago, getString(R.string.grant_park_location), getString(R.string.things_to_do_2)));
-        things_to_do.set(2, new Location(getResources(), R.drawable.humboldt_park, getString(R.string.humboldt_park_location), getString(R.string.things_to_do_3)));
-        things_to_do.set(3, new Location(getResources(), R.drawable.ice_skating, getString(R.string.millenium_park_location), getString(R.string.things_to_do_4)));
-        things_to_do.set(4, new Location(getResources(), R.drawable.lincoln_park_zoo, getString(R.string.lincoln_park_location), getString(R.string.things_to_do_5)));
-        things_to_do.set(5, new Location(getResources(), R.drawable.navy_pier, getString(R.string.streeterville_location), getString(R.string.things_to_do_6)));
-        things_to_do.set(6, new Location(getResources(), R.drawable.riverwalk, getString(R.string.the_privatebank_theatre_location), getString(R.string.things_to_do_7)));
-        things_to_do.set(7, new Location(getResources(), R.drawable.shedd_aquarium, getString(R.string.adlter_planetarium_location), getString(R.string.things_to_do_8)));
-        things_to_do.set(8, new Location(getResources(), R.drawable.willis_tower_skydeck, getString(R.string.the_privatebank_theatre_location), getString(R.string.things_to_do_9)));
+        // Populate list of attractions around Chicago
+        final ArrayList<Location> attractions = new ArrayList<>(); // Contains all attractions
+        attractions.add(0, new Location(getResources(), R.drawable.garfield_park_conservatory, getString(R.string.east_garfield_park_location), getString(R.string.attraction_1)));
+        attractions.add(1, new Location(getResources(), R.drawable.the_606, getString(R.string.humboldt_park_location), getString(R.string.attraction_2)));
+        attractions.add(2, new Location(getResources(), R.drawable.wrigley_field_baseball_and_softball, getString(R.string.wrigleyville_location), getString(R.string.attraction_3)));
+        attractions.add(3, new Location(getResources(), R.drawable.the_second_city_comedy_club, getString(R.string.lincoln_park_location), getString(R.string.attraction_4)));
 
-        // Pass all things to do data and context to adapter
-        LocationAdapter placesAdapter = new LocationAdapter(things_to_do, getContext());
+        // Pass all attractions data and context to adapter
+        LocationAdapter placesAdapter = new LocationAdapter(attractions, getContext());
 
         /**
          * Sets layout manager for recycler view
@@ -61,7 +56,7 @@ public class ThingsToDoFragment extends Fragment {
             @Override
             public void onClick(View view, int position) {
                 // Opens up clicked location in google maps
-                Location chosenPlace = things_to_do.get(position);
+                Location chosenPlace = attractions.get(position);
                 String address = chosenPlace.getmAttractionName() + "," + chosenPlace.getmLocationName();
                 Uri mapUri = Uri.parse("geo:0,0?q=" + Uri.encode(address));
                 Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
